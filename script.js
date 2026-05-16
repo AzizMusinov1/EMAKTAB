0f1d4520c8db2635a42e34208676dadfbe97ec4a
AzizMusinov1 <azizmusinov309@gmail.com>
2026-05-16 06:52:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const tests = [
+    {
+        question: "Столица Узбекистана?",
+        answers: ["Самарканд", "Ташкент", "Бухара"],
+        correct: 1
+    },
+    
+    {
+        question: "2 + 2",
+        answers: ["3", "4", "5"],
+        correct: 1
+    }
+    ];
